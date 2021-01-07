2e5c508227a8190ed213cf7fa570f53171084a43
unknown <tayanesilvasouzas@gmail.com>
2021-01-06 22:09:06 -0300

    sistema de grid

---
+++ index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thay Souza</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="container-fluid primeiro">
+
+<!--<nav class="menu">
+	<ul>
+		<li><a href="#js">Sobre mim</a></li>
+		<li><a href="#ux">Educação</a></li>
+		<li><a href="#css">Blog</a></li>
+		<li><a href="#web">Projetos</a></li>
+		<li><a href="#ui">Contato</a></li>
+	</ul>
+</nav>-->
+
+
+  
+
+
+
+
+
+<nav class="menu navbar navbar-expand-lg navbar-light bg-light  fixed-top">
+    <a class="navbar-brand" href="#home">Thay souza</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#sobremim">Sobre mim</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#educacao">Educação</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projetos">Projetos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contato">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+
+
+<div class="container-fluid p-0 ">
+
+  <div class="row" id="home">
+  <div class="imagem-fundo col-sm border">
+    <br>
+    <br>
+    <br>  
+  imagem de fundo - Parallax
+    </div>
+  </div>
+
+
+  <div class="row" >
+    <div class=" parte col-sm border">
+  foto e informações
+    </div>
+    <div class=" parte col-sm border">
+    skills
+    </div>
+  </div>
+
+  
+  <div class="row" id="sobremim">
+    <div class=" parte col-sm-4 border">
+  imagem gif
+    </div>
+    <div class=" parte col-sm border">
+   sobre mim
+    </div>
+  </div>
+
+  <div class="row" id="educacao">
+    <div class=" parte col-sm border">
+  educacao
+    </div>
+  </div>
+
+
+<!---PROJETOS-->
+<div class="row"  id="projetos"><!--TITULO-->
+    <div class=" col-sm-12 border">
+   PROJETOS
+    </div>
+  </div>
+  <!--FIM TITULO-->
+
+
+  <!--PROJETO 1-->
+  <div class="row">
+    <div class="  col-sm border"><!--parte 1-->
+
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a </p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, </p>
+    </div>
+  </div>
+</div>
+ </div>
+
+    <div class=" col-sm border"><!--parte 2-->
+   
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a </p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, </p>
+    </div>
+  </div>
+</div>
+ </div>
+
+<!--PROJETO 2-->
+ <div class="row">
+    <div class="  col-sm border"><!--parte 1-->
+
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a </p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, </p>
+    </div>
+  </div>
+</div>
+ </div>
+
+    <div class=" col-sm border"><!--parte 2-->
+   
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a </p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img class="card-img-top" src="./img/site.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, </p>
+    </div>
+  </div>
+</div>
+ </div>
+
+
+    
+    </div>
+  </div> <!--Fim row projetos-->
+<!---////////////////////////////PROJETOS-->
+
+
+<div class="row" id="contato">
+    <div class=" parte col-sm border">
+contato
+    </div>
+  </div>
+  
+
+
+
+
+
+  <footer>
+   <div class="container">
+   <div class="row">
+   <div class="col-md-2">
+   <img src="imagens/tec.png" width="60">
+   </div>
+
+   <div class="col-md-2">
+   <h4>company</h4>
+   <ul class="navbar-nav ml-auto">
+      <li class="nav-item"><a href="">sobre</a></li>
+      <li class="nav-item"><a href="">sobre</a></li>
+      <li class="nav-item"><a href="">sobre</a></li>
+      <li class="nav-item"><a href="">sobre</a></li>
+   </ul>
+    </div>
+
+    <div class="col-md-2">
+      <h4>comunidades</h4>
+      <ul class="navbar-nav ml-auto">
+         <li class="nav-item"><a href="">sobre</a></li>
+         <li class="nav-item"><a href="">sobre</a></li>
+         <li class="nav-item"><a href="">sobre</a></li>
+         <li class="nav-item"><a href="">sobre</a></li>
+      </ul>
+       </div>
+
+       <div class="col-md-2">
+        <h4>links uteis</h4>
+        <ul class="navbar-nav ml-auto">
+           <li class="nav-item"><a href="">sobre</a></li>
+           <li class="nav-item"><a href="">sobre</a></li>
+           <li class="nav-item"><a href="">sobre</a></li>
+           <li class="nav-item"><a href="">sobre</a></li>
+        </ul>
+         </div>
+
+        <div class="col-md-4">
+          <a href="" class="btn btn-outline-light">
+            <i class="fas fa-laptop-code fa-lg"></i>
+          </a>
+      
+          <a href="" class="btn btn-outline-light">
+            <i class="fab fa-facebook fa-lg"></i>
+          </a>
+      
+          <a href="" class="btn btn-outline-light">
+            <i class="fab fa-whatsapp fa-lg"></i>
+          </a>
+          </div>
+   </div>
+   </div>
+</footer>
+
+
+
+</div><!--Fim row principal-->
+
+
+
+<div class="parte" id="blog">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+	
+</div>
+
+<div class="parte" id="projetos">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+
+</div>
+
+<div class="parte" id="contato">
+  <h1>Contato</h1>
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+	
+</div>
+
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+  
+    
+
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+  
+    
+
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+  
+    
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+  
+    
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+    deserunt mollit anim id est laborum."
+	
+    </div>
+<script type="text/javascript" src="app.js"></script>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+</html>
