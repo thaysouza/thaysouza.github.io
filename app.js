f2c3f1bbbebee41ea5a60df1c88128f6691ffa81
unknown <tayanesilvasouzas@gmail.com>
2021-03-15 18:35:10 -0300

    animacao scroll

--- app.js
+++ app.js
@@ -59,3 +59,47 @@ function smoothScrollTo(endX, endY, duration) {
     window.scroll(newX, newY);
   }, 1000 / 60); // 60 fps
 };
+
+
+
+/***////////////////////////////////////////////////**/
+
+
+
+
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 200));
+}
