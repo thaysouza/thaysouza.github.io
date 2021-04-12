c2afc6041fa0d9d871b5747c45df9abfc4ea3c08
thaysouza <tayanesilvasouzas@gmail.com>
2021-04-12 03:28:29 -0300

    modif-elementos

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// Identificar o clique no menu
-// Verificar o item que foi clicado e fazer referência com o alvo
-// Verificar a distância entre o alvo e o topo
-// Animar o scroll até o alvo
-
 const menuItems = document.querySelectorAll('.menu a[href^="#"]');
 
-menuItems.forEach(item => {
-  item.addEventListener('click', scrollToIdOnClick);
-})
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
 
 function getScrollTopByHref(element) {
-  const id = element.getAttribute('href');
+  const id = element.getAttribute("href");
   return document.querySelector(id).offsetTop;
 }
 
@@ -21,19 +16,9 @@ function scrollToIdOnClick(event) {
 }
 
 function scrollToPosition(to) {
-  // window.scroll({
-  //   top: to,
-  //   behavior: "smooth",
-  // });
   smoothScrollTo(0, to);
 }
 
-/**
- * Smooth scroll animation
- * @param {int} endX: destination x coordinate
- * @param {int} endY: destination y coordinate
- * @param {int} duration: animation duration in ms
- */
 function smoothScrollTo(endX, endY, duration) {
   const startX = window.scrollX || window.pageXOffset;
   const startY = window.scrollY || window.pageYOffset;
@@ -41,12 +26,12 @@ function smoothScrollTo(endX, endY, duration) {
   const distanceY = endY - startY;
   const startTime = new Date().getTime();
 
-  duration = typeof duration !== 'undefined' ? duration : 400;
+  duration = typeof duration !== "undefined" ? duration : 400;
 
-  // Easing function
   const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
   };
 
   const timer = setInterval(() => {
@@ -57,8 +42,5 @@ function smoothScrollTo(endX, endY, duration) {
       clearInterval(timer);
     }
     window.scroll(newX, newY);
-  }, 1000 / 60); // 60 fps
-};
-
-
-
+  }, 1000 / 60);
+}
